d54624c3d0c9a32efa86269321713b343c60228b
WillKirk <willkirkpatrick097@gmail.com>
2026-05-24 23:01:34 +0100

    editing database connections ready for deployment

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,12 +1,12 @@
 import mysql from 'mysql2/promise'
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  uri: process.env.DATABASE_URL,
   waitForConnections: true,
   connectionLimit: 10,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 })
 
 export default pool
